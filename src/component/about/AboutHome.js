71d4218df443fd0e6c83027480eaf44af2c4ce86
Godfrey Bongomin <98063606+bongomin256@users.noreply.github.com>
2023-06-25 10:26:01 -0700

    added viewport height to homepage

--- src/component/about/AboutHome.js
+++ src/component/about/AboutHome.js
@@ -8,7 +8,7 @@ import { GrMysql } from "react-icons/gr";
 
 const AboutHome = () => {
   return (
-    <section className="px-5 md:flex md:flex-row-reverse md:gap-8 md:px-12 lg:px-32 lg:gap-32 mb-20">
+    <section className=" h-[70vh] px-5 md:flex md:flex-row-reverse md:gap-8 md:px-12 lg:px-32 lg:gap-32 mb-16 lg:items-center">
       <img
         className="rounded-full w-[250px] h-[250px] m-auto mb-5 lg:w-[250px] lg:h-[250px] border-4 border-pink-main md:m-0 md:w-full"
         src={myphoto}
--- src/component/projects/ProjectHome.js
+++ src/component/projects/ProjectHome.js
@@ -1,11 +1,8 @@
-// import { Button1, Button2 } from "../constants/btn/Button";
-
-// import projects from "../../data.json";
 import ProjectFeactured from "../constants/projectFeactured";
 
 const ProjectHome = () => {
   return (
-    <section className="px-5 md:px-12 lg:px-32 mb-20">
+    <section className="px-5 md:px-12 lg:px-32 mb-48">
       <div>
         <h2 className=" text-center mb-8 text-xl before:content-[''] before:h-[2px] before:block before:bg-gray-main after:content-[''] after:h-[2px] after:block after:bg-gray-main grid grid-cols-3 items-center gap-2 md:gap-0">
           <span>
